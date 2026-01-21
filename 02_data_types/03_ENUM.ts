6a9910fb7a00ad20c67e65171c007281cb8b7090
rajkishor <kothiyalrajkishor1@gmail.com>
2026-01-21 11:27:11 +0530

    we can make predefine values

---
+++ 02_data_types/03_ENUM.ts
@@ -0,0 +1,18 @@
+//enum used for predefined values like i have created
+
+enum Roles{
+    ADMIN = "admin",
+    USER = "user",
+    SUPER_ADMIN = "SUPER"
+}
+
+Roles.SUPER_ADMIN
+
+enum StatusCodes{
+    ERROR = 500,
+    ERR_HANDLER = 404,
+    SUCCESS = 200
+}
+StatusCodes.SUCCESS
+StatusCodes.ERR_HANDLER
+StatusCodes.ERROR
